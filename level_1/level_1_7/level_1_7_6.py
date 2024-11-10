3c882439eef48c4937db163ae614ac8763329add
LyovkinIlya <lyovkin2001@mail.ru>
2024-11-10 22:23:09 +0300

    level_1_7

---
+++ level_1/level_1_7/level_1_7_6.py
@@ -0,0 +1,13 @@
+num = 12345
+list_ = []
+for i in list(str(num)):
+    list_.append(int(i))
+
+print(sum(list_))
+
+'''
+Дано некоторое число:
+12345
+
+Найдите сумму цифр этого числа.
+'''
--- test.py
+++
@@ -1,2 +0,0 @@
-n = "МЫШЬ"
-print(n.lower())
